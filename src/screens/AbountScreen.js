24aa6da608626e1e57d98fb8b856b70e20162990
bilimerdi <orunerdi186@gmail.com>
2022-12-17 03:13:29 +0300

    Updated AbountScreen and finish

--- src/screens/AbountScreen.js
+++ src/screens/AbountScreen.js
@@ -5,20 +5,23 @@ import { Feather } from "@expo/vector-icons";
 const AbountScreen=({navigation})=>{
         return(
         <View style={styles.container}>
-            <View style={{padding:40,marginTop:80}}>
-                <View>
+            <View style={{padding:40}}>
+                <View style={styles.ImageBorderStyle}>
+                    <Text style={styles.AppNameText}>QUİZWORD</Text>
                     <Image
                         source={require('../../assets/image/Developer.jpg')}
                         style={styles.ImageStyle}
                     />
+                    <Text style={styles.NameText}>Erdi ÖRÜN</Text>
+                    <Text>Junior React Native Developer</Text>
                 </View>
                 <View style={styles.BorderTextStyle}>
                     <Text style={styles.AbountStyle}>
                             QuizWord mobil uygulaması; İngilizce kelimelerin türkçe karşılıklarını
-                            bulmanızı isteyen sorularla dolu bir uygulamadır.Her doğru soruda 10 puan 
-                            kazanılır ve sonunda da skor ve kelimelerin türkçe karşılıkları sonuç ekranında
+                            bulmanızı isteyen sorularla dolu bir quiz uygulamasıdır.Her doğru soruda 10 puan 
+                            kazanılır ve sonunda da skor ve kelimelerin Türkçe karşılıkları sonuç ekranında
                             görüntülenilir.Uygulama mobil uygulama geliştirme dersi final ödevi için hazırlanılmıştır.
-                            Geliştiricinin sosyal medya hesabına ve uygulamanın kodlarına (Github) ulaşabilirsiniz.
+                            Geliştiricinin sosyal medya hesabına ve uygulamanın kodlarına aşağıdaki linklerden (Github) ulaşabilirsiniz.
 
                     </Text>
                 </View>
@@ -47,20 +50,49 @@ const styles=StyleSheet.create({
         backgroundColor:"#64DFDF",
         height:"100%",
     },
+    AppNameText:{
+        fontSize:60,
+        marginBottom:10,
+        color:"#2196F3",
+        fontWeight:"800",
+        borderColor:"#2196F3",
+        borderWidth:1,
+        paddingLeft:5,
+        paddingRight:5
+    },
+    ImageBorderStyle:{
+        alignItems:"center",
+        paddingLeft:20,
+        padding:30,
+        borderRadius: 10,
+        marginTop:0,
+    },
     ImageStyle:{
         width:150,
         height:150,
+    },
+    NameText:{
+        marginTop:10,
+        fontSize:30
     },
     BorderTextStyle:{
         borderRadius:30,
         backgroundColor:"#2196F3",
         padding:20,
-        marginBottom:20,
-        marginTop:60
+        marginBottom:10,
+        /*add shadow*/
+        shadowColor: "black",
+        shadowOffset: {
+            width: 0,
+            height: 12,
+        },
+        shadowOpacity: 100,
+        shadowRadius: 50.00,
+        elevation: 24,                
     },
     AbountStyle:{
         textAlign:"center",
-        fontSize:18
+        fontSize:20
     },
     ButtonStyle:{
         alignItems:"center",
@@ -92,7 +124,7 @@ const styles=StyleSheet.create({
         borderRadius:50,
         marginRight:20,
         marginLeft:100,
-        marginBottom:20,
+        marginBottom:10,
     },
     GitHubStyle:{
         color:"#171515",
