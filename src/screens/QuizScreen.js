70234d8a5a87c06e5715370ee386c30a1354913a
bilimerdi <orunerdi186@gmail.com>
2022-12-17 04:39:01 +0300

    Update QuizScreen.js
    Added elements to quiz screen

--- src/screens/QuizScreen.js
+++ src/screens/QuizScreen.js
@@ -1,18 +1,76 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity} from "react-native";
 
 const QuizScreen=()=>{
     return(
-        <View>
-            <Text style={styles.textStyle}>This is quiz screen</Text>
+        <View style={styles.container}>
+            <View style={styles.questionBorderStyle}>
+                <Text style={styles.questionTextStyle}>Aşağıdaki kelimenin türkçe karşılığı hangi şıkta doğru verilmiştir?</Text>
+            </View>
+            <View style={styles.wordBorderStyle}>
+                <Text style={styles.wordTextStyle}>Word</Text>
+            </View>
+            <View style={styles.optionBorderStyle}>
+                <TouchableOpacity style={styles.optionStyle}>
+                    <Text style={styles.optionTextStyle}>Option 1</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.optionStyle}>
+                    <Text style={styles.optionTextStyle}>Option 2</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.optionStyle}>
+                    <Text style={styles.optionTextStyle}>Option 3</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.optionStyle}>
+                    <Text style={styles.optionTextStyle}>Option 4</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
 
 const styles=StyleSheet.create({
-    textStyle:{
-        paddingTop:50,
+    container:{
+        paddingTop:30,
     },
+    questionBorderStyle:{
+        margin:30,
+        alignItems:"center",
+        backgroundColor:"#2196F3",
+        borderRadius:16,
+        padding:20,
+    },
+    questionTextStyle:{
+        fontSize:35,
+        textAlign:"center",
+    },
+    wordBorderStyle:{
+        margin:100,
+        marginTop:20,
+        marginBottom:20,
+        alignItems:"center",
+        backgroundColor:"red",
+        borderRadius:16,
+        padding:15,
+    },
+    wordTextStyle:{
+        fontSize:40,
+        textAlign:"center",
+    },
+    optionBorderStyle:{
+        marginTop:50
+    },
+    optionStyle:{
+        margin:30,
+        marginTop:-10,
+        alignItems:"center",
+        backgroundColor:"#2196F3",
+        borderRadius:16,
+        padding:15,
+    },
+    optionTextStyle:{
+        fontSize:25,
+        textAlign:"center",
+    }
 });
 
 export default QuizScreen;
